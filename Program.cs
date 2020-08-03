b71c694679f26933a6932f611a6662de09f66bed
SiAvMaAnR <samarkin200220022002@gmail.com>
2020-08-03 17:51:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Обучение_Git
+{
+	class Program
+	{
+		static decimal factorial(int N,ref string STR)
+		{
+			if (N != 0)
+			{
+				return N * factorial(N - 1,ref STR);
+			}
+			else
+				return 1;
+		}
+
+		static void Main(string[] args)
+		{
+			string STR="";
+			int N=int.Parse(Console.ReadLine());
+			Console.WriteLine(factorial(N,ref STR));
+		}
+	}
+}
